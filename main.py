a9e63cd132fe2361b8ffc5cc384e5e31a03c07a2
Santa Abele <98461068+santaabele@users.noreply.github.com>
2022-01-26 12:10:54 +0000

    nē

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ mala=float(input("Ievadi malas garumu centimetros"))
 s1=mala**2
 s2=(mala+5)**2
 starpiba=s2-s1
-print("hey")
 s_izmaina=(starpiba/s1)*100
 print(mala,s1,s2,s_izmaina)
 print(f"Kvadrāta laukums mainījās par {round(s_izmaina,2)} procentiem")
